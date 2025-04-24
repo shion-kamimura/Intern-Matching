40065a92ddcd49ee8577aefada330ca23b5cfc92
ONのPC\shion <shion.kamimura25@gmail.com>
2025-04-24 19:31:30 +0900

    first commit

---
+++ fuel/app/logs/2025/04/17.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2025-04-17 18:15:34 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
